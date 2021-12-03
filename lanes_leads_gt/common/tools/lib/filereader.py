b2fbe776175d1b0138ed0e7c239d06a431f06ac9
Rustam Abdumalikov <abdumali@rocket.hpc.ut.ee>
2021-12-03 05:48:29 +0200

    remove folder

--- lanes_leads_gt/common/tools/lib/filereader.py
+++
@@ -1,7 +0,0 @@
-from common.url_file import URLFile
-
-def FileReader(fn, debug=False):
-  if fn.startswith("http://") or fn.startswith("https://"):
-    return URLFile(fn, debug=debug)
-  else:
-    return open(fn, "rb")
