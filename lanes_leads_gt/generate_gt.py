a43bec9ba3ccc9b3ff55fc9b8cbe2dc46b3135fd
Rustam Abdumalikov <abdumali@rocket.hpc.ut.ee>
2021-12-04 04:35:03 +0200

    working generation on HPC

--- lanes_leads_gt/generate_gt.py
+++ lanes_leads_gt/generate_gt.py
@@ -4,14 +4,17 @@ from lanes_leads_gt import generate_ground_truth
 import os
 
 
-text_files = glob.glob("/gpfs/space/home/abdumali/comma2k19/**/video.hevc", recursive = True)
-supercombo = ort.InferenceSession('supercombo.onnx')
 
+options = ort.SessionOptions() 
+options.intra_op_num_threads = 10 
+#options.inter_op_num_threads = 1
 
+supercombo = ort.InferenceSession('supercombo.onnx', providers=["CUDAExecutionProvider"], sess_options=options)
+text_files = glob.glob("/gpfs/space/projects/Bolt/comma_recordings/comma2k19/**/video.hevc", recursive = True)
 
 for file in text_files:
-    main_split = file.split( '|' )
-    print(main_split)
+    #main_split = file.split( '|' )
+    #print(main_split)
     
     #if main_split:
         #print('been')
