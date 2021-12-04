4c4f65e31483e73b3935e3be6b080759d2303ae0
Rustam Abdumalikov <abdumali@rocket.hpc.ut.ee>
2021-12-04 04:36:16 +0200

    removed redundant code

--- lanes_leads_gt/generate_gt.py
+++ lanes_leads_gt/generate_gt.py
@@ -3,8 +3,6 @@ import onnxruntime as ort
 from lanes_leads_gt import generate_ground_truth
 import os
 
-
-
 options = ort.SessionOptions() 
 options.intra_op_num_threads = 10 
 #options.inter_op_num_threads = 1
@@ -12,23 +10,7 @@ options.intra_op_num_threads = 10
 supercombo = ort.InferenceSession('supercombo.onnx', providers=["CUDAExecutionProvider"], sess_options=options)
 text_files = glob.glob("/gpfs/space/projects/Bolt/comma_recordings/comma2k19/**/video.hevc", recursive = True)
 
-for file in text_files:
-    #main_split = file.split( '|' )
-    #print(main_split)
-    
-    #if main_split:
-        #print('been')
-        #splits = file.split('/')
-        #file2 = '/'.join( splits[:-2] )
-
-        #newfolder = file2.replace('|', '_')
-   
-        #os.rename( file2, newfolder )
-        #file = newfolder + '/'.join(splits[-2:])
-    
+for file in text_files:   
     print( "In processing ...", file )
-    
-    #tdy:
+   
     generate_ground_truth( file, supercombo )
-    #except:
-    # 	print( "Exception" )
