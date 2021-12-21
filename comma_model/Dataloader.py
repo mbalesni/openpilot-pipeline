074517693323dc9242cc9c8052cd427d97234110
Mykyta Baliesnyi <nikita.balesny@gmail.com>
2021-12-21 16:48:55 +0200

    cache segment paths for speedup

--- comma_model/Dataloader.py
+++ comma_model/Dataloader.py
@@ -14,9 +14,15 @@ import glob
 import h5py
 from tqdm import tqdm
 
+
+cache_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'cache')
+path_to_videos_cache = os.path.join(cache_folder, 'videos.txt')
+path_to_gts_cache = os.path.join(cache_folder, 'ground_truths.txt')
+
+
 class CommaLoader(Dataset):
 
-    def __init__(self, recordings_path ,npz_paths, split_per, data_type, train= False, val = False):
+    def __init__(self, recordings_basedir ,npz_paths, split_per, data_type, train= False, val = False):
         super(CommaLoader, self).__init__()
         """
         Dataloader for Comma model train. pipeline
@@ -28,7 +34,7 @@ class CommaLoader(Dataset):
         
         Args: ------------------
         """
-        self.recordings_path = recordings_path
+        self.recordings_basedir = recordings_basedir
         self.data_type = data_type
         self.npz_paths = npz_paths 
         self.train = train
@@ -38,7 +44,7 @@ class CommaLoader(Dataset):
         if self.npz_paths is None:
             raise TypeError("add dummy data paths")
 
-        elif self.recordings_path is None:
+        elif self.recordings_basedir is None:
             raise TypeError("recordings path is wrong")
 
         elif self.data_type == "dummy":
@@ -59,22 +65,7 @@ class CommaLoader(Dataset):
 
         elif self.data_type == "gen_gt":
 
-            ## I can make this loop faster somewhat If i have all the converted data on the disk ## check only .plan files and look for 1190 size
-            gt_files_exist_path = sorted(glob.glob(self.recordings_path + "/**/marker_and_leads_ground_truth.npz", recursive= True))
-
-            self.hevc_file_paths = []
-            self.gt_file_paths = []
-            print("loading the respective paths")
-            for paths in tqdm(gt_files_exist_path):
-                dir_path, file_name = os.path.split(paths)
-                check_data = np.load(paths)
-                if check_data['plans'].shape[0] == 1190 or check_data['plans'].shape[0] > 1190: ## adding only those files who has more than 1190 samples
-                    # print(check_data['plans'].shape[0]
-                    if any(fname.endswith('.hevc') for fname in os.listdir(dir_path)):
-                        for file in os.listdir(dir_path):
-                            if file == "fcamera.hevc" or file == "video.hevc":
-                                self.hevc_file_paths.append(os.path.join(dir_path,file))
-                                self.gt_file_paths.append(os.path.join(dir_path,file_name))
+            self.hevc_file_paths, self.gt_file_paths = get_paths(self.recordings_basedir)
 
             print("paths loaded")
             print("length of video files", len(self.hevc_file_paths))
@@ -176,11 +167,71 @@ class CommaLoader(Dataset):
             data_gt_prob = torch.from_numpy(data_gt_prob)
             return datayuv, (data_gt, data_gt_prob)
 
+
+def get_segment_dirs(base_dir):
+    '''Get paths to segments that have ground truths.'''
+
+    gt_files = sorted(glob.glob(base_dir + "/**/marker_and_leads_ground_truth.npz", recursive=True))
+    return sorted(list(set([os.path.dirname(f) for f in gt_files])))
+
+
+def get_paths(base_dir):
+    '''Get paths to videos and ground truths. Cache them for future reuse.'''
+
+	os.makedirs(cache_folder, exist_ok=True)
+
+	if os.path.exists(path_to_videos_cache) and os.path.exists(path_to_gts_cache):
+		print('Using cached paths to videos and GTs...')
+		video_paths = []
+		gt_paths = []
+		with open(path_to_videos_cache, 'r') as f: video_paths = f.read().splitlines()
+		with open(path_to_gts_cache, 'r') as f: gt_paths = f.read().splitlines()
+	else:
+		print('Resolving paths to videos and GTs...')
+		segment_dirs = get_segment_dirs(base_dir)
+
+		# prevent duplicate writes
+		with open(path_to_videos_cache, 'w') as video_paths: pass
+		with open(path_to_gts_cache, 'w') as gt_paths: pass
+
+		gt_filename = 'marker_and_leads_ground_truth.npz'
+		video_filenames = ['fcamera.hevc', 'video.hevc']
+
+		video_paths = []
+		gt_paths = []
+
+		for segment_dir in tqdm(segment_dirs):
+
+			gt_data = np.load(os.path.join(segment_dir, gt_filename))
+
+			if gt_data['plans'].shape[0] >= 1190:  # keep segments that have >= 1190 samples
+
+				video_files = os.listdir(segment_dir)
+				video_files = [file for file in video_files if file in video_filenames]
+
+				found_one_video = 0 <= len(video_files) <= 1
+
+				if found_one_video:
+					with open(path_to_videos_cache, 'a') as video_paths_f:
+						video_path = os.path.join(segment_dir, video_files[0])
+						video_paths.append(video_path)
+						video_paths_f.write(video_path + '\n') # cache it
+
+					with open(path_to_gts_cache, 'a') as gt_paths_f:
+						gt_path = os.path.join(segment_dir, gt_filename)
+						gt_paths.append(gt_path)
+						gt_paths_f.write(gt_path + '\n') # cache it
+				else:
+					print(f'WARNING: found {len(video_files)} in segment: {segment_dir}')
+
+	return video_paths, gt_paths
+
+
 # if __name__ == "__main__":
-#     comma_recordings_path = "/gpfs/space/projects/Bolt/comma_recordings"
+#     comma_recordings_basedir = "/gpfs/space/projects/Bolt/comma_recordings"
 
 #     numpy_paths = ["inputdata.npz","gtdata.npz"]
-#     comma_data = CommaLoader(comma_recordings_path, numpy_paths, 0.8, "gen_gt",train= True)
+#     comma_data = CommaLoader(comma_recordings_basedir, numpy_paths, 0.8, "gen_gt",train= True)
 #     comma_loader = DataLoader(comma_data, batch_size=2, num_workers=2, shuffle= False)
     
 #     print("checking the shapes of the loader outs")
