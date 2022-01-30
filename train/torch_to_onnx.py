fb73b34f96be0002d575ec91ede792ee1b7dbe8d
Mykyta Baliesnyi <nikita.balesny@gmail.com>
2022-01-30 17:38:16 +0200

    fix old torch2onnx conversion bugs

--- train/torch_to_onnx.py
+++ train/torch_to_onnx.py
@@ -3,6 +3,8 @@ import warnings
 warnings.filterwarnings("ignore", category=UserWarning,
                         message='The given NumPy array is not writeable, and PyTorch does not support non-writeable tensors')
 warnings.filterwarnings("ignore", category=UserWarning, message='Using experimental implementation that allows \'batch_size > 1\'')
+warnings.filterwarnings("ignore", message='Converting a tensor to a Python boolean might cause the trace to be incorrect')
+warnings.filterwarnings("ignore", message='Iterating over a tensor might cause the trace to be incorrect')
 
 import onnx
 import onnxruntime as rt
@@ -11,11 +13,11 @@ import os
 import torch
 import numpy as np
 
-from model import load_model, ORIGINAL_MODEL
+from model import load_trainable_model, ORIGINAL_MODEL
 
 
 def save_torch_to_onnx(path_to_model):
-    torch_model = load_model(ORIGINAL_MODEL)
+    torch_model = load_trainable_model(ORIGINAL_MODEL)
     torch_model.load_state_dict(torch.load(path_to_model))
     torch_model.eval()
 
@@ -28,15 +30,19 @@ def save_torch_to_onnx(path_to_model):
 
     torch_out = torch_model(**inputs)
 
-    torch.onnx.export(torch_model, (inputs['input_imgs'], inputs['desire'], inputs['traffic_convention'], inputs['initial_state'], {}), path_to_output, export_params=True, opset_version=9, do_constant_folding=False,
-                      input_names=['input_imgs', 'desire', 'traffic_convention', 'initial_state'], output_names=['outputs'],
+    torch.onnx.export(torch_model, 
+                      (inputs['input_imgs'], inputs['desire'], inputs['traffic_convention'], inputs['initial_state'], {}), 
+                      path_to_output, 
+                      export_params=True, 
+                      opset_version=9, 
+                      do_constant_folding=False,
+                      input_names=['input_imgs', 'desire', 'traffic_convention', 'initial_state'], 
+                      output_names=['outputs'],
                       dynamic_axes={'input_imgs': {0: 'batch_size'}, 
                                     'desire': {0: 'batch_size'},
                                     'traffic_convention': {0: 'batch_size'},
                                     'initial_state': {0: 'batch_size'},
                                     'outputs': {0: 'batch_size'}})
-
-    print('Converted!')
 
     return inputs, torch_out
 
@@ -57,17 +63,16 @@ def verify_exported_model(path_exported_model, torch_ins, torch_out):
     # compare ONNX Runtime and PyTorch results
     np.testing.assert_allclose(to_numpy(torch_out), ort_outs[0], rtol=1e-03, atol=1e-05)
 
-    print("Exported model has been tested with ONNXRuntime, and the result looks good!")
-
 
 if __name__ == '__main__':
     path_to_model = sys.argv[1]
     path_to_output = sys.argv[2] if len(sys.argv) > 2 else '.'.join(path_to_model.split('.')[:-1]) + '.onnx'
 
     # convert
+    print('Saving ONNX model to {}...'.format(path_to_output))
     torch_ins, torch_out = save_torch_to_onnx(path_to_model)
 
     # verify
     verify_exported_model(path_to_output, torch_ins, torch_out)
 
-    sys.exit(0)
+    os._exit(0)
